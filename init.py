5a43d37a6ec4b6aa9ae0332f3dc60298ce7e50df
Isabel Moore <101134975+isabelmoore@users.noreply.github.com>
2025-09-24 17:50:16 -0700

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,25 @@
-
-(py311mdao) C:\Users\N81446> C:/Users/N81446/.conda/envs/py311mdao/python.exe c:/Users/N81446/misslemdao/tools/traj_ann/init_cond_opt/train_init_cond.py
 seed alphas: (0.0, 9.0)
 built 9 cases with step 0.25
+multiprocessing.pool.RemoteTraceback:
+"""
 Traceback (most recent call last):
-  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 126, in <module>
+  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 125, in worker
+    result = (True, func(*args, **kwds))
+                    ^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 48, in mapstar
+    return list(map(*args))
+           ^^^^^^^^^^^^^^^^
+  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 65, in _worker
+    os.chdir(repo_root_str)
+TypeError: chdir: path should be string, bytes or os.PathLike, not list
+"""
+
+The above exception was the direct cause of the following exception:
+
+Traceback (most recent call last):
+  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 129, in <module>
     runner.run()
-  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 110, in run
+  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 113, in run
     results = pool.map(worker_fn, cases)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^
   File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 367, in map
@@ -13,13 +27,4 @@ Traceback (most recent call last):
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 774, in get
     raise self._value
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 540, in _handle_tasks
-    put(task)
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\connection.py", line 206, in send
-    self._send_bytes(_ForkingPickler.dumps(obj))
-                     ^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\reduction.py", line 51, in dumps
-    cls(buf, protocol).dump(obj)
-AttributeError: Can't pickle local object 'AlphaGridRunner.run.<locals>.<lambda>'
-
-(py311mdao) C:\Users\N81446>
+TypeError: chdir: path should be string, bytes or os.PathLike, not list
