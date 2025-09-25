a1058dcc9a5d08e94034989de04ef2d533678282
Isabel Moore <101134975+isabelmoore@users.noreply.github.com>
2025-09-24 21:02:42 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,30 +1,15 @@
-seed alphas: (0.0, 9.0)
-built 9 cases with step 0.25
-multiprocessing.pool.RemoteTraceback:
-"""
-Traceback (most recent call last):
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 125, in worker
-    result = (True, func(*args, **kwds))
-                    ^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 48, in mapstar
-    return list(map(*args))
-           ^^^^^^^^^^^^^^^^
-  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 65, in _worker
-    os.chdir(repo_root_str)
-TypeError: chdir: path should be string, bytes or os.PathLike, not list
-"""
+       with open(p.get_outputs_dir() / "SNOPT_print.out", encoding="utf-8", errors='ignore') as f:
+            SNOPT_history = f.read()
 
-The above exception was the direct cause of the following exception:
+        # Define where the exit code information starts
+        exit_code_start = SNOPT_history.rfind("SNOPTC EXIT")
+        exit_code_end = SNOPT_history.find("\n", exit_code_start)
 
-Traceback (most recent call last):
-  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 129, in <module>
-    runner.run()
-  File "c:\Users\N81446\misslemdao\tools\traj_ann\init_cond_opt\train_init_cond.py", line 113, in run
-    results = pool.map(worker_fn, cases)
-              ^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 367, in map
-    return self._map_async(func, iterable, mapstar, chunksize).get()
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "C:\Users\N81446\.conda\envs\py311mdao\Lib\multiprocessing\pool.py", line 774, in get
-    raise self._value
-TypeError: chdir: path should be string, bytes or os.PathLike, not list
+        # Extract the exit code line
+        exit_code = int((SNOPT_history[exit_code_start:exit_code_end]).split()[2])
+
+        # TO DO: fix this tomatch that of major iterattions, not minor
+        iter_code_start = SNOPT_history.rfind("No. of iterations")
+        iter_code_end = SNOPT_history.find("\n", iter_code_start)
+
+        iterations = int((SNOPT_history[iter_code_start:iter_code_end]).split()[3])
